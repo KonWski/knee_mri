0a2e1609fe5c9c30b3c1da0145900adbf43761cb
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-05-27 00:05:36 +0200

    typo

--- model_training.py
+++ model_training.py
@@ -170,7 +170,7 @@ def train_model(device, root_dir, view_type, abnormality_type, pretrained_model_
                 model.eval()
 
             for id, batch in enumerate(dataloader, 0):
-                logging.info(f"Batch: {batch}")
+                logging.info(f"Batch: {id}")
                 images, labels = batch
                 images = images.to(device)
                 labels = labels.to(device)
