0b4061d583761333fee3f730f7c9965615ae27ea
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-05-30 18:37:56 +0200

    added resnet34, alexnet

--- pretrained_models.py
+++ pretrained_models.py
@@ -3,22 +3,32 @@ from torch import nn
 
 def get_pretrained_model(model_name: str):
     '''
-    Downloads pretrained model from PyTorch, changes its last layer
-    and returns it.
+    Downloads pretrained model from PyTorch, modifies its layers to output features
 
     Parameters
     ----------
     model_name: str
         indicator which of PyTorch pretrained model should be returned
+        possible options: resnet18, resnet34, alexnet
     '''
-
-    if model_name:
+    
+    if model_name == "resnet18":
         model = models.resnet18(pretrained=True)
         model.fc = nn.Identity()
 
-        for param in model.parameters():
-            param.requires_grad = False
+    elif model_name == "resnet34":
+        model = models.resnet34(pretrained=True)
+        model.fc = nn.Identity()
+
+    elif model_name == "alexnet":
+        model = models.alexnet(pretrained=True)
+        model.avgpool = nn.Identity()
+        model.classifier = nn.Identity()
+
     else:
         raise Exception(f"Pretrained model type not found: {model_name}")
+
+    for param in model.parameters():
+        param.requires_grad = False
 
     return model
