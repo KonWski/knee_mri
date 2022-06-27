8aafd0b310b8cc57649df7862ec67f328bd1ba27
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-06-27 23:30:54 +0200

    model change

--- models.py
+++ models.py
@@ -28,7 +28,7 @@ class ViewMriNet(nn.Module):
         self.avg_pooling_layer = nn.AdaptiveAvgPool2d((None, 3))
         self.max_pooling_layer = nn.AdaptiveMaxPool2d((None, 3))
         self.flatten = nn.Flatten()
-        self.classifier = nn.Linear(288, 1)
+        self.classifier = nn.Linear(228, 1)
 
     def forward(self, x):
 
