928414164af20eb0d098f0bc51769997c8d74a8e
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-07-11 19:45:46 +0200

    data augmentation change

--- transforms.py
+++ transforms.py
@@ -4,8 +4,9 @@ import torchvision.transforms as transforms
 # basic transformations + augmentation
 train_transforms = transforms.Compose([
     transforms.ToTensor(),
-    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(20),
     transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
+    transforms.RandomHorizontalFlip(),
     transforms.Lambda(lambda x: torch.unsqueeze(x, dim=0)),
     transforms.Lambda(lambda x: x.permute(2, 0, 1, 3)),
     transforms.Lambda(lambda x: x.repeat(1, 3, 1, 1))
