5cfb44ec4d7fce7d01450688af3b6ad51321f8f9
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-07-13 22:31:16 +0200

    experiment

--- view_model_training.py
+++ view_model_training.py
@@ -93,12 +93,12 @@ class ViewDataset(data.Dataset):
         return len(self.labels)
 
     def __getitem__(self, index):
-        print(f"index: {index}")
+        # print(f"index: {index}")
         image_row = self.labels.loc[index]
         label = image_row["abnormality"]
-        print(f"label: {label}")
+        # print(f"label: {label}")
         image_index = image_row["id"]
-        print(f"image_index: {image_index}")
+        # print(f"image_index: {image_index}")
         image = np.load(f"{self.dataset_path}/{image_index}.npy")
 
         if self.transform:
@@ -120,9 +120,9 @@ class ViewDataset(data.Dataset):
         pos = len(self.labels[self.labels["abnormality"] == 1])
         neg = len(self.labels[self.labels["abnormality"] == 0])
 
-        pos_weight = neg / pos
+        # pos_weight = neg / pos
         # return torch.tensor([3.77])
-        return torch.tensor([pos_weight])
+        return torch.tensor([1, neg / pos])
 
 
 def train_model(device, root_dir: str, view_type: str, abnormality_type: str, transfer_learning_type: str,
@@ -187,8 +187,8 @@ def train_model(device, root_dir: str, view_type: str, abnormality_type: str, tr
             len_dataset = len(dataset)
 
             if use_weights:
-                pos_weights = dataset.weights.to(device)
-                criterion.pos_weight = pos_weights
+                weights = dataset.weights.to(device)
+                criterion.weight = weights
 
             if state == "train":
                 model.train()
@@ -196,7 +196,7 @@ def train_model(device, root_dir: str, view_type: str, abnormality_type: str, tr
                 model.eval()
 
             for id, batch in enumerate(dataloader, 0):
-                print(f"id: {id}")
+                # print(f"id: {id}")
                 with torch.set_grad_enabled(state == 'train'):
 
                     # progress bar
