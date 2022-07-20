f7e6eb05e862969a5599e80e19e058e75294cd0f
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-07-20 22:41:52 +0200

    check

--- transforms.py
+++ transforms.py
@@ -8,9 +8,15 @@ train_transforms = transforms.Compose([
     transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
     transforms.RandomHorizontalFlip(),
     transforms.Lambda(lambda x: torch.unsqueeze(x, dim=0)),
-    transforms.Lambda(lambda x: x.repeat(3, 1, 1, 1)),
-    transforms.Lambda(lambda x: x.permute(1, 0, 2, 3))
+    
+    transforms.Lambda(lambda x: x.permute(2, 0, 1, 3)),
+    transforms.Lambda(lambda x: x.repeat(1, 3, 1, 1))
+    
+    # transforms.Lambda(lambda x: x.repeat(3, 1, 1, 1)),
+    # transforms.Lambda(lambda x: x.permute(1, 0, 2, 3))
     ])
+
+
 
 # basic transformations
 test_transforms = transforms.Compose([
