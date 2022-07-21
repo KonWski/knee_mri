90f2bddf97ffedbfc8a22835dad0bdb9f13d51a6
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-07-21 02:36:58 +0200

    debug

--- transforms.py
+++ transforms.py
@@ -1,6 +1,5 @@
 import torch
 import torchvision.transforms as transforms
-from torchsample.transforms import RandomRotate, RandomTranslate, RandomFlip, ToTensor, Compose, RandomAffine
 
 # transforms.Lambda(lambda x: x.permute(2, 0, 1, 3)),
 # transforms.Lambda(lambda x: x.repeat(1, 3, 1, 1))
@@ -16,9 +15,9 @@ train_transforms = transforms.Compose([
     # transforms.Lambda(lambda x: x.permute(1, 0, 2, 3))
     
     transforms.Lambda(lambda x: torch.Tensor(x)),
-    RandomRotate(25),
-    RandomTranslate([0.11, 0.11]),
-    RandomFlip(),
+    transforms.RandomRotation(20),
+    transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
+    transforms.RandomHorizontalFlip(),
     transforms.Lambda(lambda x: x.repeat(3, 1, 1, 1).permute(1, 0, 2, 3)),
 
     ])
