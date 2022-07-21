b1222f54b399b2aa964c12e5ec5537ddeea39d29
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-07-21 10:23:04 +0200

    transformation changes

--- transforms.py
+++ transforms.py
@@ -4,8 +4,8 @@ import torchvision.transforms as transforms
 # basic transformations + augmentation
 train_transforms = transforms.Compose([
     transforms.Lambda(lambda x: torch.Tensor(x)),
-    transforms.RandomRotation(20),
-    transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
+    transforms.RandomRotation(25),
+    transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
     transforms.RandomHorizontalFlip(),
     transforms.Lambda(lambda x: x.repeat(3, 1, 1, 1).permute(1, 0, 2, 3))
     ])
