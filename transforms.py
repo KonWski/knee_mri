065acc0b9d6e52e1e83ec184502621382b1b5296
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-07-21 02:33:38 +0200

    debug

--- transforms.py
+++ transforms.py
@@ -1,19 +1,25 @@
 import torch
 import torchvision.transforms as transforms
 
+# transforms.Lambda(lambda x: x.permute(2, 0, 1, 3)),
+# transforms.Lambda(lambda x: x.repeat(1, 3, 1, 1))
+
 # basic transformations + augmentation
 train_transforms = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.RandomRotation(20),
-    transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
-    transforms.RandomHorizontalFlip(),
-    # transforms.Lambda(lambda x: torch.unsqueeze(x, dim=0)),
+    # transforms.ToTensor(),
+    # transforms.RandomRotation(20),
+    # transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
+    # transforms.RandomHorizontalFlip(),
+    # transforms.Lambda(lambda x: torch.unsqueeze(x, dim=0)),    
+    # transforms.Lambda(lambda x: x.repeat(3, 1, 1, 1)),
+    # transforms.Lambda(lambda x: x.permute(1, 0, 2, 3))
     
-    # transforms.Lambda(lambda x: x.permute(2, 0, 1, 3)),
-    # transforms.Lambda(lambda x: x.repeat(1, 3, 1, 1))
-    
-    transforms.Lambda(lambda x: x.repeat(3, 1, 1, 1)),
-    transforms.Lambda(lambda x: x.permute(1, 0, 2, 3))
+    transforms.Lambda(lambda x: torch.Tensor(x)),
+    transforms.RandomRotate(25),
+    transforms.RandomTranslate([0.11, 0.11]),
+    transforms.RandomFlip(),
+    transforms.Lambda(lambda x: x.repeat(3, 1, 1, 1).permute(1, 0, 2, 3)),
+
     ])
 
 
