7b0684dd8fe64cf68834e9d5940ff7223ded2618
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-07-21 02:18:08 +0200

    debug

--- transforms.py
+++ transforms.py
@@ -7,7 +7,7 @@ train_transforms = transforms.Compose([
     transforms.RandomRotation(20),
     transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
     transforms.RandomHorizontalFlip(),
-    transforms.Lambda(lambda x: torch.unsqueeze(x, dim=0)),
+    # transforms.Lambda(lambda x: torch.unsqueeze(x, dim=0)),
     
     # transforms.Lambda(lambda x: x.permute(2, 0, 1, 3)),
     # transforms.Lambda(lambda x: x.repeat(1, 3, 1, 1))
